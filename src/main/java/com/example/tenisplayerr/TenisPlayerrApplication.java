2d21480bdb9a070638274898d73c06f2789e46bf
ISParashkevov18 <ISParashkevov18@codingburgas.bg>
2022-11-09 14:56:54 +0200

    Commit 1

---
+++ src/main/java/com/example/tenisplayerr/TenisPlayerrApplication.java
@@ -0,0 +1,13 @@
+package com.example.tenisplayerr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenisPlayerrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenisPlayerrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tenisplayerr/TenisPlayerrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tenisplayerr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenisPlayerrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
