91dc4d686e2f2f4e6c4f70bcb8df9a89d7ccd755
ISParashkevov18 <ISParashkevov18@codingburgas.bg>
2022-11-09 14:59:05 +0200

    Commit 2

--- src/main/java/com/example/tenisplayerr/TenisPlayerrApplication.java
+++ src/main/java/com/example/tenisplayerr/TenisPlayerrApplication.java
@@ -1,13 +1,34 @@
 package com.example.tenisplayerr;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class TenisPlayerrApplication {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+@SpringBootApplication
+public class TenisPlayerrApplication implements CommandLineRunner {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    PlayerDao dao;
     public static void main(String[] args) {
         SpringApplication.run(TenisPlayerrApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("Inserting Player 4: {}",dao.insertPlayer( new Player(4, "Thiem", "Austria", new Date(System.currentTimeMillis()),17)));
+
+        String Date1 ="31/12/1998";
+
+        logger.info("Updating Player with Id 4: {}", dao.updatePlayer(
+                new Player(4, "Thiem" , "Austria" , new SimpleDateFormat("dd/MM/yyyy").parse(Date1),17)));
+
+        logger.info("All Players Data: {}",dao.getAllPlayers());
+    }
 }
